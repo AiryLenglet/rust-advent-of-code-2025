2ea822ed39fb6895c2fb9d5714196ea1208727d7
Airy LENGLET <airy@lenglet.ch>
2026-02-08 11:05:55 +0100

    feat(day-1): add rotate ability to Dial

--- day-1/src/lib.rs
+++ day-1/src/lib.rs
@@ -10,6 +10,16 @@ impl Dial {
     fn read_position(&self) -> u8 {
         self.current_position
     }
+
+    fn rotate(&mut self, rotation: Rotation) {
+        self.current_position +=  match rotation {
+            Rotation::Right(position) => position,
+        }
+    }
+}
+
+enum Rotation {
+    Right(u8),
 }
 
 #[cfg(test)]
@@ -19,5 +29,12 @@ mod tests {
     #[test]
     fn test_initial_dial_position() {
         assert_eq!(Dial::new().read_position(), 50);
+    }
+
+    #[test]
+    fn test_rotate_dial() {
+        let mut dial = Dial::new();
+        dial.rotate(Rotation::Right(1));
+        assert_eq!(dial.read_position(), 51);
     }
 }
