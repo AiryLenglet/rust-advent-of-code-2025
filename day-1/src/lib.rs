4b1ddcda16953763189c355bafab33ba490434bc
Airy LENGLET <airy@lenglet.ch>
2026-02-08 10:52:30 +0100

    feat(day-1): create Dial struct

--- day-1/src/lib.rs
+++ day-1/src/lib.rs
@@ -1,5 +1,15 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+struct Dial {
+    current_position: u8,
+}
+
+impl Dial {
+    fn new() -> Self {
+        Self { current_position: 50 }
+    }
+
+    fn read_position(&self) -> u8 {
+        self.current_position
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +17,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_initial_dial_position() {
+        assert_eq!(Dial::new().read_position(), 50);
     }
 }
