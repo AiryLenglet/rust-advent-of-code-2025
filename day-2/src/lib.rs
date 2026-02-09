83c19887c398286e4c2ef3c67d6289b1a4ba1614
Airy LENGLET <airy@lenglet.ch>
2026-02-09 20:13:25 +0100

    feat(day-2): impl mini game solution

--- day-2/src/lib.rs
+++ day-2/src/lib.rs
@@ -1,23 +1,23 @@
 #[derive(Debug, PartialEq)]
 struct IdRange {
-    start: u16,
-    end: u16,
+    start: u32,
+    end: u32,
 }
 
 impl IdRange {
     fn parse(id_range: &str) -> Self {
         let (start_str, end_str) = id_range.split_once('-')
             .expect("Malformed id {id_range}");
-        let start = start_str.parse::<u16>().unwrap();
-        let end = end_str.parse::<u16>().unwrap();
+        let start = start_str.parse::<u32>().unwrap();
+        let end = end_str.parse::<u32>().unwrap();
         Self {
             start,
             end,
         }
     }
 
-    fn iter(&self) -> impl Iterator<Item = u16> {
-        (self.start..=self.end).into_iter()
+    fn iter(self) -> impl Iterator<Item = u32> {
+        self.start..=self.end
     }
 }
 
@@ -57,5 +57,27 @@ mod tests {
         assert_eq!(invalid(11), Some(11));
         assert_eq!(invalid(99), Some(99));
         assert_eq!(invalid(3446456), None);
+    }
+
+    #[test]
+    fn test_mini_game() {
+        let raw_ranges = vec![
+            "11-22",
+            "95-115",
+            "998-1012",
+            "1188511880-1188511890",
+            "222220-222224",
+            "1698522-1698528",
+            "446443-446449",
+            "38593856-38593862"
+        ];
+        
+        let result = raw_ranges.iter()
+            .map(|str_range| IdRange::parse(str_range))
+            .flat_map(|id_range| id_range.iter())
+            .map(|id| invalid(id))
+            .map(|id| id.unwrap_or_else(|| 0))
+            .reduce(|a, b| a + b);
+        assert_eq!(result, Some(1227775554));
     }
 }
