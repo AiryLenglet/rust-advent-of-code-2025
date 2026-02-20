283c831e0b29dcaa48748b6c27f72fcbdc9eef57
Airy LENGLET <airy@lenglet.ch>
2026-02-20 21:04:50 +0100

    feat(day-3): implement final solution

--- day-3/src/lib.rs
+++ day-3/src/lib.rs
@@ -1,60 +1,56 @@
-#[derive(Debug, PartialEq)]
-struct BatteryBank {
-    labels: String,
+
+fn find_first_highest(str: &str) -> (u64, u64) {
+    let mut highest = 0;
+    let mut index = 0;
+    for (i, c) in str.chars().enumerate() {
+        let char = c.to_digit(10).expect("Found invalid char");
+        if char > highest {
+            highest = char;
+            index = i;
+        }
+    }
+    (highest as u64, index as u64)
 }
 
-impl BatteryBank {
-    fn parse(labels: &str) -> Self {
-        Self {
-            labels: labels.to_string(),
-        }
+const battery_to_select: usize = 12;
+
+fn find_best_jotage(str: &str) -> u64 {
+    let mut jotage = String::new();
+    let mut start = 0;
+    while jotage.len() < battery_to_select {
+        let (highest, position) = find_first_highest(&str[start..=str.len() - battery_to_select + jotage.len()]);
+        start += position as usize + 1;
+        jotage.push_str(highest.to_string().as_str());
     }
 
-    fn find_best_jotage(&self) -> u32 {
-        let labels = self.labels.chars()
-            .map(|c| c.to_digit(10).ok_or("non‑digit char found {c}"))
-            .collect::<Result<Vec<u32>,_>>()
-            .expect("");
-        let mut first_label: u32 = 0;
-        let mut first_label_position = 0;
-        for (i, &label) in labels[0..labels.len()-1].iter().enumerate() {
-            if label >= first_label {
-                first_label = label;
-                first_label_position = i;
-            }
-        }
-
-        let mut second_label: u32 = 0;
-        for (i, &label) in labels[first_label_position + 1..labels.len()].iter().enumerate() {
-            if label >= second_label {
-                second_label = label;
-            }
-        }
-
-        10 * first_label + second_label
-    }
+    jotage.parse::<u64>().expect("Couldn't parse jotage")
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::BatteryBank;
+    use std::fs;
+    use crate::{find_best_jotage, find_first_highest,};
 
     #[test]
-    fn test_parse_battery_bank() {
-        let bank = BatteryBank::parse("123456789");
-        assert_eq!(
-            bank,
-            BatteryBank {
-                labels: String::from("123456789")
-            }
-        );
+    fn test_find_first_highest() {
+        assert_eq!(find_first_highest("12345"), (5, 4));
+        assert_eq!(find_first_highest("98765943219"), (9, 0));
     }
 
     #[test]
-    fn test_find_best_jotage() {
-        assert_eq!(BatteryBank::parse("987654321111111").find_best_jotage(), 98);
-        assert_eq!(BatteryBank::parse("811111111111119").find_best_jotage(), 89);
-        assert_eq!(BatteryBank::parse("234234234234278").find_best_jotage(), 78);
-        assert_eq!(BatteryBank::parse("818181911112111").find_best_jotage(), 92);
+    fn test_find_best_jotage_() {
+        assert_eq!(find_best_jotage("987654321111111"), 987654321111);
+        assert_eq!(find_best_jotage("811111111111119"), 811111111119);
+        assert_eq!(find_best_jotage("234234234234278"), 434234234278);
+        assert_eq!(find_best_jotage("818181911112111"), 888911112111);
+    }
+
+    #[test]
+    fn test_solution() {
+        let input = fs::read_to_string("./resource/input.txt").expect("Failed to read input file.");
+        let result = input.lines()
+            .map(|line| find_best_jotage(line))
+            .reduce(|a, b| a + b);
+        assert_eq!(result, Some(168627047606506));
     }
 }
