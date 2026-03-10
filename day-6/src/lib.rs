e29a9c2058d40a66dcb0c8a3211393a594ed95f0
Airy LENGLET <airy@lenglet.ch>
2026-03-10 19:09:54 +0100

    feat(day-6): impl solution part 2

--- day-6/src/lib.rs
+++ day-6/src/lib.rs
@@ -1,7 +1,7 @@
 use std::fmt::{Debug, Display, Formatter};
 
-struct Worksheet<'a> {
-    cells: Vec<Vec<&'a str>>,
+struct Worksheet {
+    cells: Vec<Vec<char>>,
     column_count: usize,
     row_count: usize,
 }
@@ -19,15 +19,23 @@ impl Display for WorksheetError {
 
 impl std::error::Error for WorksheetError {}
 
+impl From<std::num::ParseIntError> for WorksheetError {
+    fn from(error: std::num::ParseIntError) -> Self {
+        Self {
+            cause: format!("{}", error),
+        }
+    }
+}
+
 type Result<T> = std::result::Result<T, WorksheetError>;
 
-impl<'a> Worksheet<'a> {
-    fn parse(input: &'a str) -> Self {
+impl Worksheet {
+    fn parse(input: &str) -> Self {
         let cells = input
             .lines()
-            .map(|line| line.split_whitespace().collect::<Vec<&str>>())
-            .collect::<Vec<Vec<&str>>>();
-        let column_count = cells[0].len();
+            .map(|line| line.chars().collect::<Vec<char>>())
+            .collect::<Vec<Vec<char>>>();
+        let column_count = cells.iter().map(|row| row.len()).max().unwrap();
         let row_count = cells.len();
         Self {
             cells,
@@ -36,32 +44,36 @@ impl<'a> Worksheet<'a> {
         }
     }
 
-    fn reduce_column(&self, column_index: usize) -> Result<u64> {
-        let reduce_fn = match self.cells[self.row_count - 1][column_index] {
-            "*" => |left, right| left * right,
-            "+" => |left, right| left + right,
-            other => {
-                return Err(WorksheetError {
-                    cause: format!("Unable to parse operator {}", other),
-                });
-            }
-        };
-        (0..self.row_count - 1)
-            .into_iter()
-            .map(|row_index| self.cells[row_index][column_index])
-            .map(|value| value.parse::<u64>().expect("Unable to parse value"))
-            .reduce(reduce_fn)
-            .ok_or(WorksheetError {
-                cause: "Worksheet not containing any columns".to_string(),
-            })
-    }
+    fn compute_grand_total(&self) -> Result<u64> {
+        let mut total: u64 = 0;
+        let mut numbers: Vec<u64> = Vec::new();
 
-    fn compute_grand_total(&self) -> u64 {
-        (0..self.column_count)
-            .into_iter()
-            .map(|column_index| self.reduce_column(column_index))
-            .flatten()
-            .sum()
+        for column_index in (0..self.column_count).rev() {
+            let mut column_number_str = String::new();
+
+            for row_index in 0..self.row_count - 1 {
+                let cell_value = self.cells[row_index]
+                    .get(column_index)
+                    .unwrap_or_else(|| &' ');
+                column_number_str.push(*cell_value);
+            }
+            if !column_number_str.trim().is_empty() {
+                let digit = column_number_str.trim().parse::<u64>()?;
+                numbers.push(digit);
+            }
+
+            let char_op = self.cells[self.row_count - 1][column_index];
+            if char_op == '*' {
+                let product = numbers.iter().product::<u64>();
+                total += product;
+                numbers.clear();
+            } else if char_op == '+' {
+                let sum = numbers.iter().sum::<u64>();
+                total += sum;
+                numbers.clear();
+            }
+        }
+        Ok(total)
     }
 }
 
@@ -78,17 +90,13 @@ mod tests {
   6 98  215 314
 *   +   *   +  ",
         );
-        assert_eq!(worksheet.reduce_column(0), Ok(33210));
-        assert_eq!(worksheet.reduce_column(1), Ok(490));
-        assert_eq!(worksheet.reduce_column(2), Ok(4243455));
-        assert_eq!(worksheet.reduce_column(3), Ok(401));
-        assert_eq!(worksheet.compute_grand_total(), 4277556);
+        assert_eq!(worksheet.compute_grand_total(), Ok(3263827));
     }
 
     #[test]
     fn test_part_1() {
         let input = fs::read_to_string("./resource/input.txt").expect("Failed to read input file.");
         let worksheet = Worksheet::parse(&input);
-        assert_eq!(worksheet.compute_grand_total(), 5524274308182);
+        assert_eq!(worksheet.compute_grand_total(), Ok(8843673199391));
     }
 }
